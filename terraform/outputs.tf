cad657fe9e29dca2f69e852c1cc7fdc7b3be7468
gsrcodebase <gramadurai01@gmail.com>
2025-02-21 23:10:29 -0600

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,15 @@
+output "lambda_function_name" {
+  description = "Name of the Lambda function"
+  value       = aws_lambda_function.eServices_email_sender.function_name
+}
+
+output "lambda_function_arn" {
+  description = "ARN of the Lambda function"
+  value       = aws_lambda_function.eServices_email_sender.arn
+}
+
+output "api_gateway_url" {
+  description = "URL of the API Gateway"
+  value       = aws_api_gateway_rest_api.eServices_api.execution_arn
+}
 
