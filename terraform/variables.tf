bafcf0e559b1417f2ea0b36f09e5a1daa5e416c9
gsrcodebase <gramadurai01@gmail.com>
2025-02-21 23:09:19 -0600

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,20 @@
 
+variable "aws_region" {
+  description = "AWS Region to deploy the resources"
+  default     = "us-east-1"   # You can change this to your preferred region
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  default     = "eServicesEmailSender"
+}
+
+variable "ses_sender_email" {
+  description = "The email address used to send emails through SES"
+  default     = "sender@example.com"
+}
+
+variable "ses_recipient_email" {
+  description = "The email address that will receive the email"
+  default     = "recipient@example.com"
+}
